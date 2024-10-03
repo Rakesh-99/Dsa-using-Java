a69273b0bd2bc3ae8e798b4848d6b65dfe46a6d1
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-10-03 16:44:42 +0530

    find max element in from an array

---
+++ com/arraydemo/MaxElement.java
@@ -0,0 +1,29 @@
+package com.arraydemo;
+import java.util.Arrays;
+
+public class MaxElement {
+    public static void main(String[] args){
+
+
+        int [] nums = {12, 3, 21, 42, 76,90, 22};
+
+        System.out.println(findMaxElem(nums));
+    }
+
+    public static int findMaxElem(int[] nums) {
+        int findMax = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < nums.length; i++){
+//            if(findMax < nums[i]){
+//                findMax = nums[i];
+//            }
+//        }
+//        or
+        for (int num : nums){
+            if(findMax < num){
+                findMax = num;
+            }
+        }
+        return  findMax;
+    }
+}
