95c16983253b4044d84afcb6d9e4043adc6c6c63
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-04 21:53:25 +0530

    linear search

---
+++ com/searching/LinearSearch.java
@@ -0,0 +1,30 @@
+package com.searching;
+
+public class LinearSearch {
+    public static void main(String[] args){
+
+        // Return the index if the item is found
+        // if item is not found return -1;
+
+        int [] arr = {10, 21, 1, 3, 99, 34, 56};
+        int target = 99;
+
+        int res = linearSearchFunc(arr, target);
+        System.out.println(res);
+    }
+
+    public static int linearSearchFunc(int[] arr, int target){
+
+        if (arr.length == 0){
+            return  -1;
+        }
+
+        for (int i = 0; i < arr.length; i++){
+
+            if (arr[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
