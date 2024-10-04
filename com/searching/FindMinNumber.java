c45fe58c29d8c4fe3da04abf41b36cb5205a5d78
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-05 00:21:01 +0530

    linear search - findMinNum

---
+++ com/searching/FindMinNumber.java
@@ -0,0 +1,24 @@
+package com.searching;
+
+public class FindMinNumber {
+    public static void main (String[] args){
+
+        int[] arr = { 87, 22, 89, 70, 1, 0, -5};
+
+        int res  = findMinValue(arr);
+        System.out.println(res);
+    }
+
+    public static int findMinValue(int[] arr){
+
+        int min_value = arr[0];
+
+        for (int i = 1 ; i < arr.length ; i++) {
+            if (min_value > arr[i]){
+                min_value = arr[i];
+            }
+        }
+            return  min_value;
+    }
+
+}
