944f7e05ae3758e45156f358012ad5c4c19a0d6c
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-07 00:32:30 +0530

    leetcode[array] easy - find person"s max wealth

---
+++ com/leetcode/RichestCustomerWealth.java
@@ -0,0 +1,45 @@
+package com.leetcode;
+
+
+//Question - https://leetcode.com/problems/richest-customer-wealth/description
+
+//You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the [ith] customer has in the [jth] bank. Return the wealth that the richest customer has.
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+//        Example :
+//
+//        Input: accounts = [[1,2,3],[3,2,1]]
+//        Output: 6
+//        Explanation:
+//        1st customer has wealth = 1 + 2 + 3 = 6
+//        2nd customer has wealth = 3 + 2 + 1 = 6
+//        Both customers are considered the richest with a wealth of 6 each, so return 6.
+
+public class RichestCustomerWealth {
+    public static void main (String[] args){
+
+        int[][] arr = {
+                {1, 2, 3},
+                {3, 2, 1},
+                {10,3}
+        };
+
+       int res = getPersonMaxWealth(arr);
+        System.out.println(res);
+    }
+    public static int getPersonMaxWealth(int[][] arr){
+        int maxWealth = Integer.MIN_VALUE;
+
+        for (int row = 0; row < arr.length; row ++){
+            int sum = 0;
+
+            for (int col = 0; col < arr[row].length; col++){
+                 sum += arr[row][col];
+            }
+            if (sum > maxWealth){
+                maxWealth = sum;
+            }
+        }
+        return maxWealth;
+    }
+}
