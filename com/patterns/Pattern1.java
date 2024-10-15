86031260bd3fc35ae4fec5d4ce6a170685d8b13c
Rakesh Kumar Parida <98577842+Rakesh-99@users.noreply.github.com>
2024-10-15 16:27:11 +0530

    pattern problem 1

---
+++ com/patterns/Pattern1.java
@@ -0,0 +1,22 @@
+package com.patterns;
+
+
+public class Pattern1 {
+
+    public static void main(String[] args) {
+
+//    *
+//    * *
+//    * * *
+//    * * * *
+
+//    print the above pattern
+
+        for (int i = 0; i < 4 ; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
